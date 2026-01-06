0a135282099e8dc8a3ae4214ebb7700dc366267c
ZeynepDinc40 <zeyneepdinc40@gmail.com>
2026-01-07 01:50:02 +0300

    Refactor bubble sort and binary search implementation

--- baloncuk_siralama.c
+++ baloncuk_siralama.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
-// Zeynep Feyza Dinç
+// zeynep feyza dinç
 // 2420171038
 
-// Baloncuk Sıralama 
-void bubbleSort(int dizi[], int n) {
+int main() {
+    int dizi[10] = {12, 34, 40, 60, 87, 93, 55, 16, 74, 11};
+    int n = 10;
     int i, j, temp;
-    for (i=0; i<n-1; i++) {
-        for (j=0; j<n-i-1; j++) {
+    int aranan = 55;
+    int sol = 0, sag = n - 1, orta;
+    int bulundu = 0;
+
+    // baloncuk Siralama
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
             if (dizi[j] > dizi[j + 1]) {
                 temp = dizi[j];
                 dizi[j] = dizi[j + 1];
@@ -15,54 +21,26 @@ void bubbleSort(int dizi[], int n) {
             }
         }
     }
-}
 
-// İkili Arama 
-int binarySearch(int dizi[], int n, int aranan) {
-    int sol = 0, sag = n - 1, orta;
-
+    // ikili Arama
     while (sol <= sag) {
         orta = (sol + sag) / 2;
 
-        if (dizi[orta] == aranan)
-            return orta;
-        else if (dizi[orta] < aranan)
+        if (dizi[orta] == aranan) {
+            bulundu = 1;
+            break;
+        } else if (dizi[orta] < aranan) {
             sol = orta + 1;
-        else
+        } else {
             sag = orta - 1;
-    }
-    return -1;
-}
-
-int main() {
-    int dizi[100], n, i, aranan, sonuc;
-
-    printf("Dizi eleman sayisini giriniz: ");
-    scanf("%d", &n);
-
-    printf("Dizi elemanlarini giriniz:\n");
-    for (i = 0; i < n; i++) {
-        scanf("%d", &dizi[i]);
+        }
     }
 
-    // Baloncuk Sıralama
-    bubbleSort(dizi, n);
-
-    printf("\nSiralanmis dizi:\n");
-    for (i = 0; i < n; i++) {
-        printf("%d ", dizi[i]);
+    if (bulundu == 1) {
+        printf("Aranan sayi dizide bulundu.\n");
+    } else {
+        printf("Aranan sayi dizide bulunamadi.\n");
     }
-
-    printf("\n\nAranacak sayiyi giriniz: ");
-    scanf("%d", &aranan);
-
-    // İkili Arama
-    sonuc = binarySearch(dizi, n, aranan);
-
-    if (sonuc != -1)
-        printf("Sayi dizide bulundu. Indeks: %d\n", sonuc);
-    else
-        printf("Sayi dizide bulunamadi.\n");
 
     return 0;
 }
